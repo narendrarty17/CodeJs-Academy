46a6886c8003ede2f8efca4590c678331b986776
narendrarty17 <narendrarty17@gmail.com>
2023-12-24 05:32:38 -0800

    changed image height

--- components/Learning.js
+++ components/Learning.js
@@ -5,7 +5,7 @@ const LearningSection = () => {
         <div className="flex flex-col items-center md:flex-row">
             <div className="w-full md:w-1/2 p-8 mb-4 md:mb-0">
                 <h2 className="text-3xl font-bold mb-4">Learn New Skills Online with Ease</h2>
-                <p className="text-gray-600">
+                <p className="text-gray-400">
                     Explore a world of knowledge with our extensive selection of online courses.
                 </p>
                 <div className="mt-4 flex flex-wrap gap-y-5 gap-x-3 md:gap-x-5">
@@ -21,7 +21,7 @@ const LearningSection = () => {
                 <img
                     src="/images/section_01/posterImage.png" // Update the image path
                     alt="Learning Section"
-                    className="absolute inset-0 w-full h-[350px] object-cover max-w-full md:w-2/3 md:h-auto"
+                    className="absolute inset-0 w-full h-auto object-cover max-w-full md:w-full md:h-auto"
                 />
             </div>
         </div>
