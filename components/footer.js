fca27eea289684c9c1964ac5a4af734f4927f2d0
narendrarty17 <narendrarty17@gmail.com>
2023-12-30 03:26:53 -0800

    changes in footer

--- components/footer.js
+++ components/footer.js
@@ -1,13 +1,13 @@
 const footer = () => {
     return (
-        <footer className="overflow-hidden pt-10 pb-5 px-10 bg-blue-500">
+        <footer className="overflow-hidden pt-10 pb-5 px-10 bg-gray-900">
             <div className="flex flex-col gap-3 items-center md:items-start md:flex-row justify-between">
                 <div className="text-white text-3xl font-bold">
                     Lets Connect there
                 </div>
-                <div className="px-6 py-2 w-[170px] h-[60px] font-medium rounded-full bg-orange-400 text-white text-xl">
+                <div className="px-6 py-2 w-[200px] h-[60px] font-medium rounded-full bg-blue-500 text-white text-xl">
                     <button className="flex justify-center items-center pl-3 pt-1.5 gap-1">
-                        <p>Hire me</p>
+                        <p>Enroll Now</p>
                         <img
                             className="w-8"
                             src="/images/footer/up-right-arrow.png"
@@ -18,9 +18,13 @@ const footer = () => {
             <div className="mt-20 w-full h-[2px] bg-gray-500" />
             <div className="mt-5 flex flex-wrap justify-center md:justify-start gap-10">
                 <div className="flex flex-col gap-4">
-                    <div className="flex gap-2">
-                        <img src="/images/footer/company_logo.svg" />
-                        <img src="/images/footer/company_name.svg" />
+                    <div className="flex items-center">
+                        <img
+                            src="/images/header/brandLogo.svg"
+                            alt="Company Logo"
+                            className="h-8 w-8 mr-2"
+                        />
+                        <span className="text-white text-2xl font-bold">CodeJS</span>
                     </div>
                     <div className="hidden md:inline text-white max-w-xl">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed congue
@@ -36,7 +40,7 @@ const footer = () => {
                     </div>
                 </div>
                 <div className="hidden md:flex flex-col gap-3">
-                    <header className="text-orange-400 font-bold">
+                    <header className="text-blue-500 font-bold">
                         Navigation
                     </header>
                     <div className="text-white flex flex-col gap-2">
@@ -48,17 +52,16 @@ const footer = () => {
                     </div>
                 </div>
                 <div className="flex flex-col gap-3">
-                    <header className="text-orange-400 font-bold">
+                    <header className="text-blue-500 font-bold">
                         Contact
                     </header>
                     <div className="text-white flex flex-col gap-2">
                         <div>+91-9919748714</div>
                         <div>narendrarty17@gmail.com</div>
-                        <div>Portfolio-Narendra</div>
                     </div>
                 </div>
                 <div className="hidden md:flex flex-col gap-3">
-                    <div className="text-orange-400 font-bold">
+                    <div className="text-blue-500 font-bold">
                         Get the latest information
                     </div>
                     <div className="flex">
@@ -66,8 +69,8 @@ const footer = () => {
                             className="rounded-l-lg pl-2 h-8"
                             placeholder="Email Address"
                         />
-                        <div className="flex justify-center items-center bg-orange-400 w-10 h-8 rounded-r-lg">
-                            <img src="images/right-ar.svg" />
+                        <div className="flex justify-center items-center bg-blue-500 w-10 h-8 rounded-r-lg">
+                            <img src="images/footer/right-ar.svg" />
                         </div>
                     </div>
                 </div>
