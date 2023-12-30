66db44f4cda87513f713bc4a73e3703a0354d380
narendrarty17 <narendrarty17@gmail.com>
2023-12-30 10:54:19 -0800

    Update Section_02.js

--- components/Section_02.js
+++ components/Section_02.js
@@ -76,7 +76,7 @@ const Section_02 = () => {
                 } else {
                     handleSwipe('left');
                 }
-            }, 2000);
+            }, 1000);
         }
 
         return () => {
