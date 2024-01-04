aecb1d736b0a1d8a3034ecb5d9aceb40c8671869
narendrarty17 <narendrarty17@gmail.com>
2024-01-04 09:16:18 -0800

    Update Section_01.js

--- components/Courses/Course/Section_01.js
+++ components/Courses/Course/Section_01.js
@@ -2,26 +2,15 @@ import React from 'react';
 
 const YouTubeEmbed = ({ embedId }) => {
     return (
-        <div className="flex flex-row justify-between aspect-w-16 aspect-h-9 w-full h-auto">
+        <div className="flex justify-center aspect-w-16 aspect-h-9 w-full h-auto">
             <iframe
                 className="w-full h-[232px] md:w-2/3 md:h-[480px] bg-gray-200"
-                src="https://www.youtube.com/embed/7NMEbvbVck8?si=4RMeKVMlID8OiSSP"
+                src="https://www.youtube.com/embed/7NMEbvbVck8?si=4RMeKVMlID8OiSSP&rel=0"
                 title="YouTube video player"
                 frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allow="accelerometer; gyroscope;"
                 allowFullScreen
             ></iframe>
-            <div
-                className="hidden lg:flex md:flex-col md:w-1/3"
-            >
-                <h1>Course Content</h1>
-                <ul>
-                    <li>Video 1: Video 1 title</li>
-                    <li>Video 2: Video 2 title</li>
-                    <li>Video 3: Video 3 title</li>
-                    <li>Video 4: Video 4 title</li>
-                </ul>
-            </div>
         </div>
     );
 };
