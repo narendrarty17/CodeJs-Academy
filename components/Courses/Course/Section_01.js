876c89eed04a6a2040c2afdb9d04f1393dd9f2c7
narendrarty17 <narendrarty17@gmail.com>
2024-01-04 08:42:53 -0800

    Update Section_01.js

--- components/Courses/Course/Section_01.js
+++ components/Courses/Course/Section_01.js
@@ -1,31 +1,35 @@
 import React from 'react';
 
-const VideoComponent = ({ embedCode, title, description }) => (
-    <div className="text-center">
-        {/* YouTube Video */}
-        <div className="max-w-screen-lg mx-auto mb-4">
-            <div className="aspect-w-16 aspect-h-9">
-                <div dangerouslySetInnerHTML={{ __html: embedCode }} className="aspect-content" />
+const YouTubeEmbed = ({ embedId }) => {
+    return (
+        <div className="flex flex-row justify-between aspect-w-16 aspect-h-9 w-full h-auto">
+            <iframe
+                className="w-full h-[235px] md:w-2/3 md:h-[480px] bg-gray-200"
+                src="https://www.youtube.com/embed/7NMEbvbVck8?si=4RMeKVMlID8OiSSP"
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen
+            ></iframe>
+            <div
+                className="hidden md:flex md:flex-col md:w-1/3"
+            >
+                <h1>Course Content</h1>
+                <ul>
+                    <li>Video 1: Video 1 title</li>
+                    <li>Video 2: Video 2 title</li>
+                    <li>Video 3: Video 3 title</li>
+                    <li>Video 4: Video 4 title</li>
+                </ul>
             </div>
         </div>
-
-        {/* Video Title */}
-        <h2 className="text-2xl font-bold mt-4">{title}</h2>
-
-        {/* Video Description */}
-        <p className="text-gray-600 mt-2">{description}</p>
-    </div>
-);
-
+    );
+};
 
 const Section_01 = () => {
-    const embedCode = '<iframe width="410" height="350" src="https://www.youtube.com/embed/7NMEbvbVck8?si=SIFAbmnJ_3X44Fg5" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
-    const title = 'Title of the Video';
-    const description = 'Description of the video goes here.';
-
     return (
-        <div className="flex justify-center">
-            <VideoComponent embedCode={embedCode} title={title} description={description} />
+        <div>
+            <YouTubeEmbed />
         </div>
     );
 };
