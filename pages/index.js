4accad803150a6b50771e8c52d5579aca22322e1
narendrarty17 <narendrarty17@gmail.com>
2024-01-17 06:44:15 -0800

    minimal changes

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,6 @@ import Footer from '@/components/utils/Footer';
 {/* Importing LandingPage Components */ }
 import Intro from '@/components/LandingPage/01_Intro';
 import OurRecruiters from '@/components/LandingPage/02_OurRecruiters';
-import DiscoverCourses from '@/components/LandingPage/03_DiscoverCourses';
 import Courses from '@/components/LandingPage/04_Courses';
 import Reviews from '@/components/LandingPage/05_Reviews';
 import Instructors from '@/components/LandingPage/06_Instructors';
