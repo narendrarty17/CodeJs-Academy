f222bc1c2256e3e1b20116a4b9e4686f7692808c
narendrarty17 <narendrarty17@gmail.com>
2024-01-31 11:41:04 -0800

    Update VideoCard.js

--- components/Blogs/homeComponents/VideoCard.js
+++ components/Blogs/homeComponents/VideoCard.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const VideoCard = () => {
     return (
-        <div className="max-w-xl md:max-w-md flex flex-row gap-5 md:gap-0 md:flex-col mx-auto">
+        <div className="max-w-lg md:max-w-md flex flex-row gap-5 md:gap-0 md:flex-col mx-auto">
             {/* Image Section */}
             <div className="mb-2">
                 <img
-                    className="w-full h-36 md:h-40 lg:h-48 object-cover"
+                    className="w-full h-32 md:h-40 lg:h-48 object-cover"
                     src="/images/blogs/blog/blog_01/blogIcon.png"
                     alt="Blog Image"
                 />
