9acc2189f41449a5770c69e5fe8feb7c2ae60301
narendrarty17 <narendrarty17@gmail.com>
2024-02-01 06:38:09 -0800

    Update CareerAdvice.js

--- components/Blogs/homeComponents/CareerAdvice.js
+++ components/Blogs/homeComponents/CareerAdvice.js
@@ -2,7 +2,7 @@ import BlogCardHorizontal from "./BlogCardHorizontal";
 
 const CareerAdvice = () => {
     return (
-        <div className=" px-8 md:px-20">
+        <div className="flexpx-8 md:px-20">
             <div className="flex gap-4 items-center">
                 <span className="bg-green-400 w-8 h-8 rounded-full" />
                 <h1 className="text-2xl">Career Advice</h1>
@@ -16,7 +16,7 @@ const CareerAdvice = () => {
                     <BlogCardHorizontal />
                     <BlogCardHorizontal />
                 </div>
-                <div className="flex flex-col md:flex-row justify-center items-center gap-6 md:gap-8">
+                <div className="hidden md:flex flex-col md:flex-row justify-center items-center gap-6 md:gap-8">
                     <BlogCardHorizontal />
                     <BlogCardHorizontal />
                 </div>
